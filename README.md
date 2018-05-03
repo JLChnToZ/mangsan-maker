63ef456fc92aeee51dcd72dd70f49fbc4fcff691
JLChnToZ <jeremylam619@gmail.com>
2018-05-04 01:22:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Mangsan Maker
+=============
+This is a toy that generates random words pattern by given the source.
+
+Usage
+-----
+This is just a single HTML file with some scripting, just simply add your words after the link with a question mark (`?`) and enjoy!
+It supports randomize "single characters" and "words", in default,
+it will randomizes by the given letters and generates output,
+but you can use the `&` to separates all of your words after the question mark of the link,
+it will switch into the words randomization mode.
+
+If you still don't have an idea on how to do it, you may have a look one of the demostration below.
+
+If you click on the generated words on the page, it will re-generate a new one for you.
+
+To share the result and let your friends to play with it,
+simply copy the whole link with the hashes (the random id after the `#`) which automatically added.
+If you just want to share the pattern you have entered but without the generated result,
+do not copy the hashes while sharing, everything will be fine.
+
+Snapshots / Demos
+-----------------
+- With some random Latin letters (ABCDE)
+  [![ABCDE](previews/1.png)](https://code.moka-rin.moe/mangsan-maker/?ABCDE)
+- With some random Chinese characters (紙包雞)
+  [![紙包雞](previews/2.png)](https://code.moka-rin.moe/mangsan-maker/?%E7%B4%99%E5%8C%85%E9%9B%9E)
+- With some random English words (Foo, Bar, Baz)
+  [![Foo Bar Baz](previews/3.png)](https://code.moka-rin.moe/mangsan-maker/?Foo&Bar&Baz)
+- With some random Japanese characters (バナナ)
+  [![バナナ](previews/4.png)](https://code.moka-rin.moe/mangsan-maker/?%E3%83%90%E3%83%8A%E3%83%8A)
+
+License
+-------
+[DBAD License](LICENSE.md)
---
+++ docs/mangsan.js
@@ -0,0 +1,108 @@
+((window, document, history, location) => {
+  const supportsHistoryState = typeof history.pushState === 'function';
+  const text = ((src, placeHolder) => {
+    const raw = src.length > 1 && src.substr(1);
+    return !raw ? placeHolder :
+      raw.indexOf('&') >= 0 ?
+      raw.split('&').map(decodeURIComponent) :
+      decodeURIComponent(raw);
+  })(location.search, '萌新');
+  const { getIndex, joinStr, title } = (src => typeof src === 'string' ? {
+    getIndex: String.prototype.charAt,
+    joinStr: '',
+    title: src
+  } : {
+    getIndex(index) { return this[index]; },
+    joinStr: ' ',
+    title: src.join(' ')
+  })(text);
+  document.title = `${title} Maker`;
+  const base = text.length;
+  const maxLength = Math.floor(Math.log(Number.MAX_SAFE_INTEGER + 1) / Math.log(base));
+  
+  const trianglify = (() => {
+    if(!window.Trianglify)
+      return function(seed) {};
+    let trianglify, timer, lastSeed = null, firstRun;
+    function run(seed) {
+      if(timer) timer = undefined;
+      if(!seed) seed = lastSeed;
+      else lastSeed = seed;
+      const opts = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        seed
+      };
+      trianglify = trianglify ?
+        Trianglify(Object.assign(trianglify.opts, opts)) : Trianglify(opts);
+      document.body.style.backgroundImage = `url(${trianglify.png()})`;
+    }
+    return function(seed) {
+      if(timer)
+        clearTimeout(timer);
+      if(!firstRun)
+        run(seed);
+      else
+        timer = setTimeout(run, 200, seed);
+      firstRun = true;
+    };
+  })();
+
+  const element = document.createElement('h1');
+  element.id = 'name';
+  element.tabIndex = 0;
+  document.body.appendChild(element);
+
+  window.addEventListener('hashchange', parseHash);
+  window.addEventListener('popstate', ({ state }) => {
+    if(state) parse(state);
+  });
+  window.addEventListener('resize', e => void trianglify());
+
+  element.addEventListener('click', e => void generate());
+  element.addEventListener('keypress', e => {
+    if(e.key !== ' ' && e.key !== 'Enter') return;
+    e.preventDefault();
+    generate();
+  });
+
+  if(!parseHash()) generate(1);
+
+  function parseHash() {
+    const { hash } = location;
+    if(typeof hash !== 'string') return;
+    const matches = hash.match(/^#([A-Z0-9]{1,2})-([A-Z0-9]{1,11})$/i);
+    if(!matches) return;
+    const state = {
+      l: parseInt(matches[1], 36),
+      v: parseInt(matches[2], 36),
+    };
+    if(supportsHistoryState)
+      history.replaceState(state, parse(state), hash);
+    return 1;
+  }
+
+  function parse({ l: length, v: value }) {
+    if(!Number.isSafeInteger(length) || !Number.isSafeInteger(value)) return;
+    value = value.toString(base);
+    value = length > value.length ?
+      '0'.repeat(length - value.length) + value :
+      value.substr(-length);
+    trianglify(`${length}/${value}`);
+    return element.textContent = value.split('').map(getIndex, text).join(joinStr);
+  }
+
+  function generate(replace) {
+    const l = Math.floor(Math.random() * maxLength + 1);
+    const v = Math.floor(Math.random() * (Math.pow(base, l) - 1));
+    const state = { l, v };
+    const parsedState = parse(state);
+    const hash = `#${l.toString(36)}-${v.toString(36)}`;
+    if(supportsHistoryState)
+      history[replace ? 'replaceState' : 'pushState'](
+        state, parsedState, hash
+      );
+    else
+      location.hash = hash;
+  }
+})(window, document, history, location);
